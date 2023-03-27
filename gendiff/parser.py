700be58241cf6b19a6fafef07a76fe55610546b5
Faizik12 <zerg9119@gmail.com>
2023-03-27 08:04:32 +0500

    Improve tests and file reading

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,5 +1,6 @@
 import json
 import yaml
+import os.path
 from yaml.loader import SafeLoader
 from gendiff.differences import make_diff
 from gendiff.formatters.stylish_formatter import stylish
@@ -7,31 +8,31 @@ from gendiff.formatters.plain_formatter import plain
 from gendiff.formatters.json_formatter import json_formatter
 
 
+def get_file_format(filepath):
+    return os.path.splitext(filepath)[1]
+
+
 def read_file(filepath):
-    json_format = 'json'
-    yaml_format = 'yaml'
-    with open(filepath, 'r') as file_1:
-        if filepath.endswith('.json'):
-            data = (file_1.read(), json_format)
-        else:
-            data = (file_1.read(), yaml_format)
+    with open(filepath, 'r') as file:
+        file_content = file.read()
+    return file_content
+
+
+def get_dict(file_content, file_format):
+    if file_format == '.json':
+        data = json.loads(file_content)
+    else:
+        data = yaml.load(file_content, Loader=SafeLoader)
     return data
 
 
-def get_dict(data):
-    text_1, format_1 = data
-    if format_1 == 'json':
-        dict_ = json.loads(text_1)
-    else:
-        dict_ = yaml.load(text_1, Loader=SafeLoader)
-    return dict_
-
-
 def generate_diff(filepath_1, filepath_2, formatter='stylish'):
-    data_1 = read_file(filepath_1)
-    data_2 = read_file(filepath_2)
-    dict_1 = get_dict(data_1)
-    dict_2 = get_dict(data_2)
+    file_format_1 = get_file_format(filepath_1)
+    file_format_2 = get_file_format(filepath_2)
+    file_content_1 = read_file(filepath_1)
+    file_content_2 = read_file(filepath_2)
+    dict_1 = get_dict(file_content_1, file_format_1)
+    dict_2 = get_dict(file_content_2, file_format_2)
     diff = make_diff(dict_1, dict_2)
     if formatter == 'stylish':
         return stylish(diff)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,42 +3,21 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    'filepath_1',
-    ['tests/fixtures/file1.json', 'tests/fixtures/file3.yml'],
-)
-@pytest.mark.parametrize(
-    'filepath_2',
-    ['tests/fixtures/file2.json', 'tests/fixtures/file4.yml'],
-)
-def test_generate_diff_with_simple_file(filepath_1, filepath_2):
-    with open('tests/fixtures/simple_result.txt', 'r') as file:
+    'filepath_1,filepath_2,filepath_3,formatter',
+    [('tests/fixtures/file1.json', 'tests/fixtures/file2.json',
+      'tests/fixtures/simple_result.txt', 'stylish'),
+     ('tests/fixtures/file3.yml', 'tests/fixtures/file4.yml',
+      'tests/fixtures/simple_result.txt', 'stylish'),
+     ('tests/fixtures/complex_file1.json', 'tests/fixtures/complex_file2.json',
+      'tests/fixtures/complex_result.txt', 'stylish'),
+     ('tests/fixtures/complex_file3.yml', 'tests/fixtures/complex_file4.yml',
+      'tests/fixtures/complex_result.txt', 'stylish'),
+     ('tests/fixtures/complex_file1.json', 'tests/fixtures/complex_file2.json',
+      'tests/fixtures/plain_result.txt', 'plain'),
+     ('tests/fixtures/complex_file1.json', 'tests/fixtures/complex_file2.json',
+      'tests/fixtures/json_result.json', 'json'),],)
+def test_generate_diff_with_simple_file(filepath_1, filepath_2,
+                                        filepath_3, formatter):
+    with open(filepath_3, 'r') as file:
         result = file.read()
-    assert generate_diff(filepath_1, filepath_2) == result
-
-
-@pytest.mark.parametrize(
-    'filepath_1',
-    ['tests/fixtures/complex_file1.json', 'tests/fixtures/complex_file3.yml'],
-)
-@pytest.mark.parametrize(
-    'filepath_2',
-    ['tests/fixtures/complex_file2.json', 'tests/fixtures/complex_file4.yml'],
-)
-def test_generate_diff_with_complex_file(filepath_1, filepath_2):
-    with open('tests/fixtures/complex_result.txt', 'r') as file:
-        result = file.read()
-    assert generate_diff(filepath_1, filepath_2) == result
-
-
-def test_generate_diff_with_plain_formate():
-    with open('tests/fixtures/plain_result.txt', 'r') as file:
-        result = file.read()
-    assert generate_diff('tests/fixtures/complex_file1.json',
-                         'tests/fixtures/complex_file2.json', 'plain') == result
-
-
-def test_generate_diff_with_json_formate():
-    with open('tests/fixtures/json_result.json', 'r') as file:
-        result = file.read()
-    assert generate_diff('tests/fixtures/complex_file1.json',
-                         'tests/fixtures/complex_file2.json', 'json') == result
+    assert generate_diff(filepath_1, filepath_2, formatter) == result
